0e9c2daddefc85274ca5afba6b896983c404a294
teojiahao <kelvinteojiahao@hotmail.com>
2020-11-29 20:00:59 +0800

    push something that is related to this repo

--- webserver.go
+++ webserver.go
@@ -5,28 +5,42 @@ import (
 	"log"
 	"net/http"
 	"strconv"
+	"text/template"
 )
 
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*"))
+}
+
 func main() {
-	http.HandleFunc("/inc", incHandler)
+	http.HandleFunc("/", index)
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
-func incHandler(w http.ResponseWriter, r *http.Request) {
-	input := r.FormValue("input")
-	if input == "" {
-		http.Error(w, "missing value", http.StatusBadRequest)
+func index(res http.ResponseWriter, req *http.Request) {
+
+	if req.Method == http.MethodPost {
+		num1, _ := strconv.Atoi(req.FormValue("number1"))
+		num2, _ := strconv.Atoi(req.FormValue("number2"))
+		buttons := req.FormValue("buttons")
+		strconv.Atoi(req.FormValue("number1"))
+		switch buttons {
+		case "+":
+			fmt.Fprintln(res, num1, buttons, num2, "=", Add(num1, num2))
+		case "-":
+			fmt.Fprintln(res, num1, buttons, num2, "=", Subtract(num1, num2))
+		case "x":
+			fmt.Fprintln(res, num1, buttons, num2, "=", Multiply(num1, num2))
+		case "/":
+			fmt.Fprintln(res, num1, buttons, num2, "=", Divide(num1, num2))
+		}
 		return
 	}
 
-	val, err := strconv.Atoi(input)
-	if err != nil {
-		http.Error(w, "not a number: "+input, http.StatusBadRequest)
-		return
-	}
-
-	fmt.Fprintln(w, val+1)
+	tpl.ExecuteTemplate(res, "index.gohtml", nil)
 }
